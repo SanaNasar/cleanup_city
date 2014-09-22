eccfaaa362157b0fe3aeec35356ce0ca380d32c5
SanaNasar <sanasehereen.cse@gmail.com>
2014-09-21 23:32:25 -0700

    Added relationships to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,29 @@
 class User < ActiveRecord::Base
   has_secure_password # for hand written rails oauth
+  
+  attr_accessor :avatar
+  has_many :posts
+  has_many :images
 
-    has_many :posts
+  has_attached_file :avatar,
+  styles:{
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  },
+    :bucket => ENV['AWS_BUCKET_NAME'],
+    :storage => :s3,
+    # :s3_host_name => 's3-us-west-1.amazonaws.com',
+    :url => 's3_domain_url',
+    :path => '/:class/:attachment:/:id_partition/:style/:filename',
+    :s3_credentials =>{
+      :access_key_id=> ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+
+
+  validates_attachment_content_type :avatar, :content_type=> /\Aimage\/.*\Z/
+
+
+
 end
